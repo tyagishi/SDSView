b5b1897810ab11cf04b3e0af9368f98aaf962391
Tomoaki Yagishita <tyagishi@gmail.com>
2023-10-28 21:44:05 +0900

    now TextEditor's text is synced with its source

--- Sources/SDSView/ScrollTextView.swift
+++ Sources/SDSView/ScrollTextView.swift
@@ -17,7 +17,7 @@ import SDSNSUIBridge
 // at iOS, NSUIScrollView will be ignored/ at macOS NSUITextView will be ignored
 public typealias ScrollTextViewFactory = @MainActor () -> (NSUITextView, NSUIScrollView)
 //public typealias ScrollTextViewSetup = @MainActor (NSUITextView, NSUIScrollView) -> Void
-public typealias ScrollTextViewUpdate = @MainActor (NSUITextView, NSUIScrollView) -> Void
+public typealias ScrollTextViewUpdate = @MainActor (NSUITextView, NSUIScrollView, String) -> Void
 
 #if os(macOS)
 import AppKit
@@ -39,44 +39,49 @@ public typealias EditActions = NSTextStorage.EditActions
 #endif
 #if os(macOS)
 public struct ScrollTextView: NSViewRepresentable {
+    public typealias NSViewType = NSScrollView
+    @Binding var text: String
     let textViewFactory: ScrollTextViewFactory
     //let textViewSetup: ScrollTextViewSetup
     let textViewUpdate: ScrollTextViewUpdate
 
-    public init(textViewFactory: @escaping ScrollTextViewFactory,
+    public init(text: Binding<String>,
+                textViewFactory: @escaping ScrollTextViewFactory,
                 //textViewSetup: @escaping ScrollTextViewSetup,
                 textViewUpdate: @escaping ScrollTextViewUpdate) {
+        self._text = text
         self.textViewFactory = textViewFactory
         //self.textViewSetup = textViewSetup
         self.textViewUpdate = textViewUpdate
     }
-
+    
     @MainActor
     public func makeNSView(context: Context) -> NSViewType {
         let (_, scrollView) = textViewFactory()
         //textViewSetup(textView, scrollView)
         return scrollView
     }
-
+    
     @MainActor
     public func updateNSView(_ scrollView: NSViewType, context: Context) {
         guard let textView = scrollView.documentView as? NSUITextView else { return }
-        textViewUpdate(textView, scrollView)
+        textViewUpdate(textView, scrollView, text)
     }
-
-    public typealias NSViewType = NSScrollView
 }
 #elseif os(iOS)
 public struct ScrollTextView: UIViewRepresentable {
     public typealias UIViewType = UITextView
+    @Binding var text: String
 
     let textViewFactory: ScrollTextViewFactory
     //let textViewSetup: ScrollTextViewSetup
     let textViewUpdate: ScrollTextViewUpdate
 
-    public init(textViewFactory: @escaping ScrollTextViewFactory,
+    public init(text: Binding<String>,
+                textViewFactory: @escaping ScrollTextViewFactory,
                 //textViewSetup: @escaping ScrollTextViewSetup,
                 textViewUpdate: @escaping ScrollTextViewUpdate) {
+        self._text = text
         self.textViewFactory = textViewFactory
         //self.textViewSetup = textViewSetup
         self.textViewUpdate = textViewUpdate
@@ -91,9 +96,8 @@ public struct ScrollTextView: UIViewRepresentable {
 
     @MainActor
     public func updateUIView(_ textView: UIViewType, context: Context) {
-        textViewUpdate(textView, textView)
+        textViewUpdate(textView, textView, text)
     }
 
 }
 #endif
-
