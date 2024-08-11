80ba0015f44d38b1f0111e9cc3f5c9451c9cf15d
Tomoaki Yagishita <tyagishi@gmail.com>
2024-08-11 21:34:34 +0900

    R!! update SwiftLint

--- Package.swift
+++ Package.swift
@@ -19,7 +19,7 @@ let package = Package(
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
         .package(url: "https://github.com/tyagishi/SDSNSUIBridge", from: "1.2.0"),
-        .package(url: "https://github.com/realm/SwiftLint.git", from: "0.55.0"),
+        .package(url: "https://github.com/SimplyDanny/SwiftLintPlugins", exact: "0.56.1"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -28,7 +28,7 @@ let package = Package(
             name: "SDSView",
             dependencies: ["SDSNSUIBridge"],
             plugins: [
-                .plugin(name: "SwiftLintBuildToolPlugin", package: "SwiftLint"),
+                .plugin(name: "SwiftLintBuildToolPlugin", package: "SwiftLintPlugins")
             ]
         ),
         .testTarget(
